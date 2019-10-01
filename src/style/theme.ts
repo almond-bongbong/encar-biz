4a9730ce16b5aa18c06ef8f1943d1d988e71bc6c
cmlee <bal.dongdongdong@gmail.com>
2019-10-01 23:53:21 +0900

    settings, tslint prettier

---
+++ src/style/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  blue: '#3498db',
+  green: '#1abc9c',
+  grey: '#7f8c8d',
+  yellow: '#f1c40f',
+};
+
+export { theme };
