0f95a922a2fd180371c4191b0ed7b4dce8c0d315
almond-bongbong <bal.dongdongdong@gmail.com>
2019-11-27 20:58:09 +0900

    add server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+const port = 8080;
+
+app.use('/static', express.static(path.resolve(__dirname, './build/static')));
+app.use('/', express.static(path.resolve(__dirname, './build')));
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(path.resolve(__dirname, './build/index.html')));
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
